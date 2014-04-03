831ef6b36a341b1f678f2146e4463a113e467127
John Pelgrift <jpelgrif@calpoly.edu>
2014-04-03 12:54:46 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "John Pelgrift"
+git config --global user.email "jpelgrif@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"

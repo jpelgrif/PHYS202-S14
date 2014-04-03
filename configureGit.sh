e81f6d5ae318a5a4df791a1973677ee2471d646d
John Pelgrift <jpelgrif@calpoly.edu>
2014-04-03 13:04:51 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+# with git on a new computer
+
 git config --global user.name "John Pelgrift"
 git config --global user.email "jpelgrif@calpoly.edu"
 git config --global color.ui "auto"
